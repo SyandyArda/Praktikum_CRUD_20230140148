2a7bfc582e5e685cefa2ab8b3d0b79098da1fc9f
Arsyan <syandi.2901@gmail.com>
2026-03-02 19:06:24 -0800

    menambahkan file serta code UserService

--- src/main/java/com/example/praktikum2/service/UserService.java
+++ src/main/java/com/example/praktikum2/service/UserService.java
@@ -1,4 +1,14 @@
 package com.example.praktikum2.service;
 
+import com.example.praktikum2.model.dto.UserAddRequest;
+import com.example.praktikum2.model.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    UserDTO AddUser(UserAddRequest request);
+    List<UserDTO> getAllUser();
+    UserDTO getUserById(String id);
+    UserDTO UpdateUser(String id, UserAddRequest request);
+    void DeleteUser(String id);
 }
